e4dc262b63f602976c7eea794f0f33088181950a
nphoff <hoffman.nathan89@gmail.com>
2015-01-31 19:47:14 -0500

    debugger isn't quite working yet, WIP

--- js/spaaace.js
+++ js/spaaace.js
@@ -3,28 +3,53 @@
     var Game = function() {
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+        this.keyboarder = new Keyboarder();
+
+        //Keeping track of frames
+        this.requestId;
 
         this.renderer = new THREE.WebGLRenderer();
         this.renderer.setSize( window.innerWidth, window.innerHeight );
         document.body.appendChild( this.renderer.domElement );
 
-        var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        //var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        var geometry = new THREE.DodecahedronGeometry(3, 1);
         var green_material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        green_material.wireframe = true;
         var cube = new THREE.Mesh( geometry, green_material );
         this.scene.add( cube );
+
 
         this.camera.position.z = 5;
 
         var tick = function () {
-            requestAnimationFrame(tick);
+            this.requestId = requestAnimationFrame(tick);
 
-            cube.rotation.x += 0.1;
-            cube.rotation.y += 0.1;
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+
+            this.update();
 
             this.renderer.render(this.scene, this.camera);
         }.bind(this);
 
         tick();
+    };
+
+    Game.prototype = {
+        update : function() {
+            if (this.keyboarder.isDown(this.keyboarder.KEYS.GENERAL.ESC)) {
+                //break from the game loop, stop the laptop toaster.
+                window.cancelAnimationFrame(this.requestId);
+                this.requestId = undefined;
+                throw "Exiting because received ESC";
+            }
+            if (this.keyboarder.isDown(this.keyboarder.KEYS.GENERAL.ENTER)) {
+                //Manual debugging, hit enter :)
+                this.keyboarder.keyUp[this.keyboarder.KEYS.GENERAL.ENTER];
+                debugger;
+            }
+        },
     };
 
     var Keyboarder = function() {
@@ -40,6 +65,10 @@
 
         this.isDown = function(keyCode) {
             return keyState[keyCode] === true;
+        };
+
+        this.keyUp = function(keyCode) {
+            keyState[keyCode] = false;
         };
 
         this.KEYS = {
